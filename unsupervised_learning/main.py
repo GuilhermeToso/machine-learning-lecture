e20c58a1c15df7b8aab76a70a03648894e81f93a
GuilhermeToso <guilhermetoso@hotmail.com>
2022-07-04 12:24:56 -0300

    Run unsupervised learning framework

---
+++ unsupervised_learning/main.py
@@ -0,0 +1,7 @@
+from unsupervised_learning import ULFramework
+
+ulframework = ULFramework()
+ulframework.load_ul_model()
+ulframework.plot_data()
+ulframework.plot_clustered_data()
+ulframework.plot_distribution()
