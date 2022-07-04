d48f44addaa0449d75650451a44efbb87131949c
GuilhermeToso <guilhermetoso@hotmail.com>
2022-07-03 23:03:33 -0300

    Add main file of supervised learning framework

---
+++ supervised_learning/main.py
@@ -0,0 +1,7 @@
+from supervised_learning import SLFramework
+
+slframework = SLFramework()
+slframework.load_sl_model()
+slframework.plot_data()
+slframework.plot_decision_boundary()
+slframework.plot_decision_boundary("test")
